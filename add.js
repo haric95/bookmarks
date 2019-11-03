49e8102c0babe3bb773f8170398de2a6b4a3e657
haric95 <hari-c@hotmail.co.uk>
2019-11-03 22:51:28 +0000

    all needed functionality there

--- add.js
+++ add.js
@@ -5,33 +5,43 @@ if (window.localStorage.getItem("bookmarks") === null) {
 }
 // parsing the object in local storage and saving it under bookmarks variable
 let localBookmarks = JSON.parse(window.localStorage.getItem("bookmarks"));
-
 let bookmarksList = document.querySelector(".bookmarks-list");
 let form = {};
 form.url = document.querySelector(".url-input");
 form.submitButton = document.querySelector(".url-submit");
 form.submitButton.addEventListener("click", addBookmark);
+let filterInput = document.querySelector(".filter-input");
+filterInput.addEventListener("input", filterBookmarks);
 
-// When the page loads, it will start on the first page.
+// When the page loads, it will start on the first page of bookmarks.
 retreiveBookmarks(1);
 
 // clears all existing <li> in bookmarksList and creates 20 new items with data from localBookmarks
-function retreiveBookmarks(page_num) {
+// takes an array of bookmarks as an argument
+// defaults to whole localBookmarks object but can pass it a sorted/filtered array.
+function retreiveBookmarks(e, bookmarks = localBookmarks) {
+    let page_num;
+    if (e === 1) {
+        page_num = 1
+    } else {
+        page_num = e.target.value;
+    }
     //these variables are used to determine which items are pulled from localBookmarks.
     //slicing into the array means that if there are not 20 links to display, it will only display the correct number of items.
     let start_index = (page_num-1)*10;
-    let end_index = start_index + localBookmarks.slice(start_index, start_index + 20).length;
+    let end_index = start_index + bookmarks.slice(start_index, start_index + 20).length;
     while (bookmarksList.hasChildNodes()) {
         bookmarksList.removeChild(bookmarksList.lastChild);
     };
     for (let i = start_index; i < end_index; i++) {
-        buildListItem(i);
+        buildListElement(i, bookmarks);
     };
+    addPageNumbers(page_num, bookmarks);
 };
 
-function buildListItem(i) {
+function buildListElement(i, bookmarks) {
     // building the html elements to display the new bookmark
-    let url = localBookmarks[i]["url"]
+    let url = bookmarks[i]["url"]
     newListItem = document.createElement("li");
     newDiv = document.createElement("div");
     newDiv.className = "bookmarks-list-item";
@@ -52,7 +62,10 @@ function buildListItem(i) {
     newEditButton = document.createElement("button");
     newEditButton.className = "list-button";
     newEditButton.innerHTML = "Edit";
+    //adding the event listeners for the edit button
+    newDeleteButton.addEventListener("click", editBookmark);
     newDiv.appendChild(newEditButton);
+    // let tagButtons = generateTags()
     
 
 };
@@ -60,17 +73,20 @@ function buildListItem(i) {
 function addBookmark(e) {
     e.preventDefault();
     let url = form.url.value;
-    if (! urlValidator(url)) {
-        console.log("Please enter a valid url.")
+    let isValid = urlValidator(url);
+    console.log(isValid)
+    if (! urlValidator(url)[0]) {
+        alert("Please enter a valid url");
     } else {
         let new_bookmark = {}
-        new_bookmark.url = url;
+        new_bookmark.url = isValid[1];
         new_bookmark.tags = [];
         new_bookmark.time = new Date();
         localBookmarks.push(new_bookmark);
         window.localStorage.setItem("bookmarks", JSON.stringify(localBookmarks));
+        window.location.href = "results.html";
     }
-    window.location.href = "results.html";
+    
 }
 
 function removeBookmark(e) {
@@ -79,7 +95,69 @@ function removeBookmark(e) {
     location.reload();
 }
 
-function urlValidator(e) {
-    return true
+function editBookmark(e) {
+    
 }
 
+function urlValidator(url) {
+    //using regex to check if URL syntax is valid
+    if (!/^http[s]?\:\/\//.test(url)) {
+        url = 'https://' + url;
+    };
+    var pattern = new RegExp('^(https?:\\/\\/)?'+ // protocol
+    '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|'+ // domain name
+    '((\\d{1,3}\\.){3}\\d{1,3}))'+ // OR ip (v4) address
+    '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*'+ // port and path
+    '(\\?[;&a-z\\d%_.~+=-]*)?'+ // query string
+    '(\\#[-a-z\\d_]*)?$','i');
+    //ensuring that the link has http protocol specified
+    
+    // currently getting cross-origin error due to Same-Origin policy
+    // could implement further checks to validate whether url exists here
+
+    // request = new XMLHttpRequest();
+    // request.onreadystatechange = function() {
+    //     console.log(request.readyState);
+    //     if (request.readyState === 4) {
+    //         return true
+    //     };
+    // };
+    // request.open("GET", url);
+    // request.send();
+    return [pattern.test(url), url];
+}
+
+function addPageNumbers(page_num, bookmarks) {
+    let pageNumContainer = document.querySelector(".page-numbers")
+    while (pageNumContainer.hasChildNodes()) {
+        pageNumContainer.removeChild(pageNumContainer.lastChild);
+    };
+    numPages = Math.ceil(bookmarks.length/10)
+    for (let i = 1; i <= numPages; i++) {
+        pageButton = document.createElement("button");
+        pageButton.className = "page-button";
+        pageButton.innerHTML = i;
+        pageButton.setAttribute("value", i);
+        if (i == page_num) {
+            pageButton.style.borderWidth = "5px";
+        }
+        pageButton.addEventListener("click", retreiveBookmarks);
+        pageNumContainer.appendChild(pageButton);
+    };
+};
+
+function filterBookmarks(e) {
+    console.log(e);
+    if (tag.length === 0) {
+        retreieveBookmarks(1,localBookmarks);
+    }
+    filteredBookmarks = localBookmarks.filter(function(bookmark) {
+        tags = bookmark["tags"]
+        if (tags.length ===  0 ) {
+            return false
+        } else {
+            return tags.contains(tag);
+        };
+    });
+    retreiveBookmarks(1, filteredBookmarks);
+}
--- results.js
+++ results.js
@@ -1,1 +1,19 @@
+//getting the last url added to the localBookmarks object
 let localBookmarks = JSON.parse(window.localStorage.getItem("bookmarks"));
+let lastUrl = localBookmarks[localBookmarks.length - 1]["url"]
+
+let submissionContainer = document.querySelector(".submission-container");
+let returnButton = document.querySelector(".return-button");
+
+
+let thankYou = document.createElement("h2")
+thankYou.innerHTML = lastUrl+ " has been added to your bookmarks!";
+submissionContainer.appendChild(thankYou);
+
+returnButton.addEventListener("click", function() {
+    window.location.href = "index.html"
+})
+
+
+
+
